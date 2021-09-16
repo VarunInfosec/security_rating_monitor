694adac1097fe614554dfe9515da39cddd525123
VarunInfosec <varun.sharma@infosecventures.com>
2021-09-16 11:56:27 +0530

    Removed authorization secret from Constant.php

--- app/Constant.php
+++ app/Constant.php
@@ -3,7 +3,7 @@
 namespace App;
 
 class Constant{
-    public const AUTHORIZATION_KEY = "Basic c3JtX2JhY2tlbmRfYXBpOnE1IUhMLylrcEItUW5ZWig=";
+    public const AUTHORIZATION_KEY = "Authorization Secret"; // Please ask for this key from your seniors.
     public const TYPE_COMPANY = 1;
     public const TYPE_VENDOR = 2;
     public const SEARCH_SCAN = "SearchScan";
